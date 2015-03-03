bffa91c3e2b3a88abd69c3de1e9f67a29ba31a04
fickludd <johan.teleman@gmail.com>
2015-03-03 13:19:39 +0100

    Added support for required params, that will crash if not explicitly set

--- src/main/scala/se/jt/Params.scala
+++ src/main/scala/se/jt/Params.scala
@@ -4,6 +4,107 @@
  */
 package se.jt
 
-class Params {
+object Params {
+	
+	class ParamException(msg:String) extends Exception(msg) {}
+	
+	case class ParamUpdater(
+			name:String, 
+			desc:String, 
+			curr:Any, 
+			update:String => Unit
+		)
+	
+	class Param[T](val default:Option[T], val desc:String) {
+		var _value = default
+		def value = 
+			_value.getOrElse { throw new ParamException("unintialized param") }
+		def value_=(t:T) = _value = Some(t)
+	}
+	
+	case class Plong(l:Long, 		override val desc:String) extends Param[Long](Some(l), desc)
+	case class Pint(i:Int, 			override val desc:String) extends Param[Int](Some(i), desc)
+	case class Pouble(d:Double, 	override val desc:String) extends Param[Double](Some(d), desc)
+	case class Pring(s:String, 		override val desc:String) extends Param[String](Some(s), desc)
+	case class Poolean(b:Boolean, 	override val desc:String) extends Param[Boolean](Some(b), desc)
+	
+	case class ReqBoolean(	override val desc:String) extends Param[Boolean](None, desc)
+	case class ReqInt(		override val desc:String) extends Param[Int](None, desc)
+	case class ReqLong(		override val desc:String) extends Param[Long](None, desc)
+	case class ReqDouble(	override val desc:String) extends Param[Double](None, desc)
+	case class ReqString(	override val desc:String) extends Param[String](None, desc)
+	/*
+	case class PoptionLong(override val desc:String) extends Param[Option[Long]](None, desc)
+	case class PoptionInt(override val desc:String) extends Param[Option[Int]](None, desc)
+	case class PoptionDouble(override val desc:String) extends Param[Option[Double]](None, desc)
+	case class PoptionString(override val desc:String) extends Param[Option[String]](None, desc)
+	case class PoptionBoolean(override val desc:String) extends Param[Option[Long]](None, desc)
+	*/
+	
+	implicit class PlongWrapper(l:Long) 		{ def ##(desc:String) = Plong(l, desc) }
+	implicit class PintWrapper(i:Int) 			{ def ##(desc:String) = Pint(i, desc) }
+	implicit class PoubleWrapper(d:Double) 		{ def ##(desc:String) = Pouble(d, desc) }
+	implicit class PringWrapper(s:String) 		{ def ##(desc:String) = Pring(s, desc) }
+	implicit class PooleanWrapper(b:Boolean) 	{ def ##(desc:String) = Poolean(b, desc) }
+	
+	
+	implicit def pbool2bool(p:Param[Boolean]) 		= p.value
+	implicit def pint2int(p:Param[Int]) 			= p.value
+	implicit def plong2long(p:Param[Long]) 			= p.value
+	implicit def pdouble2double(p:Param[Double]) 	= p.value
+	implicit def pstring2string(p:Param[String]) 	= p.value
+	/*
+	implicit def poption2bool(p:Poption[Boolean]) = 
+		p.value.getOrElse { throw new ParamException("unintialized param") }
+	implicit def poption2int(p:Poption[Int]) = 
+		p.value.getOrElse { throw new ParamException("unintialized param") }
+	implicit def poption2long(p:Poption[Long]) = 
+		p.value.getOrElse { throw new ParamException("unintialized param") }
+	implicit def poption2double(p:Poption[Double]) = 
+		p.value.getOrElse { throw new ParamException("unintialized param") }
+	implicit def poption2string(p:Poption[String]) = 
+		p.value.getOrElse { throw new ParamException("unintialized param") }
 
+*/ 
+}
+
+
+
+trait Params {
+	
+	import Params._
+	
+	private def isParam(c:java.lang.Class[_]):Boolean =
+		if 		(c == classOf[AnyRef]) 		false
+		else if (c == classOf[Param[_]]) 	true
+		else 								isParam(c.getSuperclass)
+	
+	def opts:Map[String, ParamUpdater] = {
+		val c = this.getClass()
+		val paramFields = c.getDeclaredFields().filter(f => 
+				!f.getType.isPrimitive() && isParam(f.getType)
+			).map(_.getName)
+		paramFields map (pf => {
+			
+			val m = c.getMethod(pf)
+			def fix[T](p:Param[T], up:String => Unit) = 
+				ParamUpdater(pf, p.desc, p._value, up)
+					
+			pf -> (m.invoke(this) match {
+				case p:Poolean => 	fix(p, str => p.value = str.toBoolean)
+				case p:Pint => 		fix(p, str => p.value = str.toInt)
+				case p:Plong => 	fix(p, str => p.value = str.toLong)
+				case p:Pouble => 	fix(p, str => p.value = str.toDouble)
+				case p:Pring => 	fix(p, str => p.value = str)
+				case p:ReqBoolean => 	fix(p, str => p.value = str.toBoolean)
+				case p:ReqInt => 		fix(p, str => p.value = str.toInt)
+				case p:ReqLong => 		fix(p, str => p.value = str.toLong)
+				case p:ReqDouble => 	fix(p, str => p.value = str.toDouble)
+				case p:ReqString => 	fix(p, str => p.value = str)
+				case _ => throw new Exception("method returning Param didn't return param... confused!")
+			})
+		}) toMap
+	}
+	
+	def desc:String = opts.values.map(pu => "%s\t%s".format(pu.name, pu.desc)).mkString("\n")
 }
---
+++ src/test/scala/se/jt/ParamsTest.scala
@@ -0,0 +1,112 @@
+package se.jt
+
+import org.scalatest.FunSuite
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class ParamsTest extends FunSuite {
+
+	import Params._
+	
+	class P1 extends Params {
+			val s = "hi"
+			val flag = true
+			val num = 100
+			val wDesc = Pint(666, "supposedly evil default number")
+		}
+	
+	class P2 extends Params {
+		val s = 	"hi" 	## "this is a string param"
+		val flag = 	true 	## ""
+		val num = 	100 	## ""
+	}
+	
+	class P3 extends Params {
+		val s 		= "hi" 	## "this is a string param"
+		val flag 	= ReqBoolean("needs to be set!")
+	}
+	
+	test("basic") {
+		val p = new P1 
+		val opts = p.opts
+		assert(1 === opts.size)
+		assert(opts contains "wDesc")
+	}
+	
+	test("implicits") {
+		val p = new P2
+		
+		val s:String = p.s
+		val flag:Boolean = p.flag
+		val num:Int = p.num
+	}
+	
+	test("update") {
+		val p = new P2
+		
+		val opts = p.opts
+		try {
+			opts("s") match {
+				case ParamUpdater(n, d, x, up) =>
+					up("hello!")
+					assert("hello!" === p.s.value)
+			}
+			
+			opts("flag") match {
+				case ParamUpdater(n, d, x, up) =>
+					up("false")
+					assert(!p.flag.value)
+			}
+			
+			opts("num") match {
+				case ParamUpdater(n, d, x, up) =>
+					up("42")
+					assert(42 === p.num.value)
+			}
+		} catch {
+			case _:Throwable =>
+				fail("Threw for valid operations. 's', 'flag',and 'num' should all be valid opts.")
+		}
+	}
+	
+	test("update2") {
+		val p = new P2
+		
+		val opts = p.opts
+		opts("s").update("hello!")
+		assert("hello!" === p.s.value)
+		
+		opts("flag").update("false")
+		assert(!p.flag)
+		
+		opts("num").update("42")
+		assert(42 === p.num.value)
+	}
+	
+	test("desc") {
+		val p = new P2
+		
+		assert(p.desc === "s\tthis is a string param\nflag\t\nnum\t")
+	}
+	
+	test("poption") {
+		val p = new P3
+		
+		val opts = p.opts
+		assert(2 === opts.size)
+		try {
+			val b:Boolean = p.flag
+			fail("did not throw exception when accessing uninitialized value")
+		} catch {
+			case pe:ParamException => {}
+			case _:Throwable => 
+				fail("threw wrong kind of exception when accessing uninitialized value")
+				
+		}
+		
+		opts("flag").update("true")
+		assert(p.flag)
+	}
+}
